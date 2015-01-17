5065f381bde4c139df42e7814b892f8375505f48
John Louie <jlouie95618@gmail.com>
2015-01-17 13:13:30 -0800

    going to start search and remove over

--- linkedlist.py
+++ linkedlist.py
@@ -27,33 +27,33 @@ class linkedlist():
 
 
 	def remove(self, data = None, node = None):
-		# find the node BEFORE desired node
-		if data != None:
-			prev = self.search(data, True)
-			prev.next_node = (prev.next_node).next_node
-		# if given the desired node to be removed
-		#	just replace the node with the next
-		elif node != None:
-			node = node.next_node
-		else:
-			print "You are not trying to remove anything."
+		# # find the node BEFORE desired node
+		# if data != None:
+		# 	prev = self.search(data, True)
+		# 	prev.next_node = (prev.next_node).next_node
+		# # if given the desired node to be removed
+		# #	just replace the node with the next
+		# elif node != None:
+		# 	node = node.next_node
+		# else:
+		# 	print "You are not trying to remove anything."
 
 	def search(self, data, flag = False): # Returns the node containing the desired data
-		curr = self.head
-		print "head node data: " + self.head.data
-		print "data to find: " + data
-		if flag == True:
-			prev = None
-			while curr.data != data:
-				if curr.next_node == None: break
-				prev = curr
-				curr = curr.next_node
-			return prev
-		else:
-			while curr.next_node != None:
-				if curr.data == data: return curr
-				curr = curr.next_node
-		print "Could not find element!" 
+		# curr = self.head
+		# print "head node data: " + self.head.data
+		# print "data to find: " + data
+		# if flag == True:
+		# 	prev = None
+		# 	while curr.data != data:
+		# 		if curr.next_node == None: break
+		# 		prev = curr
+		# 		curr = curr.next_node
+		# 	return prev
+		# else:
+		# 	while curr.next_node != None:
+		# 		if curr.data == data: return curr
+		# 		curr = curr.next_node
+		# print "Could not find element!" 
 
 
 	def printList(self):
